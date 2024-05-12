a4ec4b6e38f68f45fcbedb5a174ed2fadabc2f45
Jon C <me@jonc.dev>
2024-05-12 18:07:17 +0200

    [SOL] Support debug builds and unwinding

--- zig/lib/std/posix.zig
+++ zig/lib/std/posix.zig
@@ -685,7 +685,7 @@ pub fn abort() noreturn {
         exit(127); // Pid 1 might not be signalled in some containers.
     }
     switch (native_os) {
-        .uefi, .wasi, .emscripten, .cuda, .amdhsa => @trap(),
+        .uefi, .wasi, .emscripten, .cuda, .amdhsa, .solana => @trap(),
         else => system.abort(),
     }
 }
