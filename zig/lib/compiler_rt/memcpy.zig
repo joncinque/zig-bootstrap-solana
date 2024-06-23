05d7435634adc4bb6cf8b197dbbc7ab0d27e0537
Jon C <me@jonc.dev>
2024-06-23 13:31:16 +0200

    [SOL] Alias mem syscalls to sol_mem variants (#3)
    #### Problem
    
    The memory syscalls (memset, memcpy, memmove, memcmp) don't exist on
    Solana. The sol_mem variants exist, but the backend doesn't know to use
    them.
    
    #### Solution
    
    Call them for the solana backend.
    
    Fixes #1

--- zig/lib/compiler_rt/memcpy.zig
+++ zig/lib/compiler_rt/memcpy.zig
@@ -4,7 +4,18 @@ const builtin = @import("builtin");
 
 comptime {
     if (builtin.object_format != .c) {
-        @export(memcpy, .{ .name = "memcpy", .linkage = common.linkage, .visibility = common.visibility });
+        if (builtin.os.tag == .solana) {
+            const Syscall = struct {
+                extern fn sol_memcpy_(noalias dest: ?[*]u8, noalias src: ?[*]const u8, len: usize) callconv(.C) void;
+                pub fn sol_memcpy(noalias dest: ?[*]u8, noalias src: ?[*]const u8, len: usize) callconv(.C) ?[*]u8 {
+                    sol_memcpy_(dest, src, len);
+                    return dest;
+                }
+            };
+            @export(Syscall.sol_memcpy, .{ .name = "memcpy", .linkage = common.linkage, .visibility = common.visibility });
+        } else {
+            @export(memcpy, .{ .name = "memcpy", .linkage = common.linkage, .visibility = common.visibility });
+        }
     }
 }
 
